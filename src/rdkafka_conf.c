7e2200f12d84d548e97635129acfe63981c9a92f
Konstantin Bogdanov <konstantin@clickhouse.com>
2026-06-23 17:37:43 +0200

    Remove curl references
    (cherry picked from commit 9b9ff81f88aac9450c72db5ad3e405fe3bda9ed6)

--- src/rdkafka_conf.c
+++ src/rdkafka_conf.c
@@ -56,10 +56,6 @@
 #include <windows.h>
 #endif
 
-#if WITH_OAUTHBEARER_OIDC
-#include <curl/curl.h>
-#endif
-
 struct rd_kafka_property {
         rd_kafka_conf_scope_t scope;
         const char *name;
@@ -4274,10 +4270,6 @@ const char *rd_kafka_conf_finalize(rd_kafka_type_t cltype,
                 return "`https.ca.location` must be "
                        "an existing file or directory";
 
-#if !CURL_AT_LEAST_VERSION(7, 77, 0)
-        if (conf->https.ca_pem)
-                return "`https.ca.pem` requires libcurl 7.77.0 or later";
-#endif
 #endif
 
 
